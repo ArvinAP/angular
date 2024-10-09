7056d75d103aef453cc3d89e3d38a94ac22ad972
pads <padua.arvin@gmail.com>
2024-10-09 13:04:11 +0800

    added app.components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component'
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostLIstComponent } from './posts/post-list/post-list.component';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent],
+    PostCreateComponent,
+    HeaderComponent,
+    PostLIstComponent,
+    ],
   imports: [
     BrowserModule,
-    FormsModule],
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatExpansionModule,
+    CdkAccordionModule,
+  ],
+  exports: [
+    BrowserModule,
+    FormsModule,],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { Post } from '../post.model';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = '';
+  enteredContent = '';
+  enteredTitle = '';
+
+  @Output() postCreated = new EventEmitter();
+
   onAddPost(){
-    this.newPost = this.enteredValue;
-    //this.newPost = 'this is from the new post under the onAddPost method/function'
+    const posts: Post = {
+      title: this.enteredTitle,
+      content: this.enteredContent,
+    };
+    this.postCreated.emit(posts);
   }
 }
